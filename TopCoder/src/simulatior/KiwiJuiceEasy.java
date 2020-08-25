13f0b1430cd89ef67aa86e9e589a4325f49255f9
sm917 <sm917@192.168.1.102>
2020-08-25 23:00:11 +0900

    20200825

---
+++ TopCoder/src/simulatior/KiwiJuiceEasy.java
@@ -0,0 +1,13 @@
+package simulatior;
+
+public class KiwiJuiceEasy {
+	public int[] thePouring(int[] capacities,int[]bottles,int[]fromId,int[]toId) {
+		int result[] = {};
+		return result;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
