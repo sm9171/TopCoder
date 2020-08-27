03ff35258fe2fe4bbfbc3d03e6ca24f6ac07220b
sm917 <sm917@192.168.1.102>
2020-08-27 19:46:36 +0900

    dd

--- TopCoder/src/simulatior/KiwiJuiceEasy.java
+++ TopCoder/src/simulatior/KiwiJuiceEasy.java
@@ -24,7 +24,7 @@ public class KiwiJuiceEasy {
 		int[] capacities={20,20};
 		int[] bottles={5,8};
 		int[] fromId={0};
-		int[] toId={1};
+		int[] toId={1}; 
 		int[] result = kiwi.thePouring(capacities,bottles ,fromId ,toId );
 		for(int i=0;i<result.length;i++) {
 			System.out.println(result[i]);
